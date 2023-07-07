5d482e8fc862064f1fc4d29860d013a29461245c
Danny Ben Shitrit <db@dannyben.com>
2023-07-07 05:18:47 +0000

    lint

--- lib/recode/refinements.rb
+++ lib/recode/refinements.rb
@@ -2,7 +2,7 @@ module Recode
   module Refinements
     refine String do
       def underscore
-        gsub(/::/, '/')
+        gsub('::', '/')
           .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
           .gsub(/([a-z\d])([A-Z])/, '\1_\2')
           .tr('-', '_')
